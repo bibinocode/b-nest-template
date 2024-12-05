ebd88b01fba66b9a867912583782e233c5b17025
阿逼 <2553400824@qq.com>
2024-12-05 21:49:15 +0800

    chore: 增加winston + nest-winston按天分割日志

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  LoggerService,
+  OnModuleInit,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  @Inject(WINSTON_MODULE_NEST_PROVIDER)
+  private readonly logger: LoggerService;
+
   constructor(private readonly appService: AppService) {}
+
+  onModuleInit() {
+    this.logger.log('AppController constructor');
+    this.logger.error('AppController constructor');
+    this.logger.warn('AppController constructor');
+    this.logger.debug('AppController constructor');
+    this.logger.verbose('AppController constructor');
+  }
 
   @Get()
   async getHello() {
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,60 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { WinstonModule, utilities as nestWinstonUtils } from 'nest-winston';
+
+import * as winston from 'winston';
+
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+console.log('🚀 ~ file: app.module.ts:13 ~ isDev:', isDev);
+
+function createDailyRotateFileTransport(level: string, fileName: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${fileName}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH', // 日期格式
+    zippedArchive: true, // 归档压缩
+    maxFiles: '14d', // 最大保存14天
+    maxSize: '20m', // 最大大小
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(), // 简单格式
+    ),
+  });
+}
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonUtils.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+        ...(isDev
+          ? []
+          : [
+              createDailyRotateFileTransport('info', 'app'),
+              createDailyRotateFileTransport('warn', 'app-warn'),
+              createDailyRotateFileTransport('error', 'app-error'),
+            ]),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
