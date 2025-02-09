7c43b856eb23fd7710fa3cfbd0cd3500bf8a4ea0
阿逼 <2553400824@qq.com>
2025-02-09 20:08:02 +0800

    feat: 全局异常处理器

---
+++ src/common/filters/all-exception.filter.ts
@@ -0,0 +1,48 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import * as requestIp from 'request-ip';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger();
+
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+  catch(exception: unknown, host: ArgumentsHost) {
+    // 获取HTTP适配器
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const msg: unknown = exception['response'] || 'Internal Server Error';
+
+    // ? 这里可以加入更多的处理逻辑
+
+    const responseBody = {
+      headers: request.headers,
+      query: request.query,
+      body: request.body,
+      params: request.params,
+      timestamp: new Date().toISOString(),
+      ip: requestIp.getClientIp(request),
+      exception: exception['name'],
+      error: msg,
+    };
+
+    this.logger.error(msg, exception);
+    // 使用HTTP适配器回复请求
+    httpAdapter.reply(response, responseBody, httpStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const Config = app.get(ConfigService);
 
   /**
    * 设置全局Logger
@@ -12,9 +14,19 @@ async function bootstrap() {
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   /**
+   * 全局异常过滤器
+   */
+  const isGlobalErrorFilterOn = Config.get<boolean>(
+    'GLOBAL_ERROR_FILTER_ON',
+    true,
+  );
+  if (isGlobalErrorFilterOn) {
+    app.useGlobalFilters(new AllExceptionFilter(app.get(HttpAdapterHost)));
+  }
+
+  /**
    * PORT配置
    */
-  const Config = app.get(ConfigService);
   const PORT = Config.get<number>('PORT', 3000);
   await app.listen(PORT);
 }
