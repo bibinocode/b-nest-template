2cbe6611121bd84a717830965979daf231384969
阿逼 <2553400824@qq.com>
2025-02-10 23:01:54 +0800

    feat: redis的集成

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import {
   Controller,
   Get,
   Inject,
   LoggerService,
   OnModuleInit,
+  Version,
 } from '@nestjs/common';
-import { AppService } from './app.service';
+import Redis from 'ioredis';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController implements OnModuleInit {
   @Inject(WINSTON_MODULE_NEST_PROVIDER)
   private readonly logger: LoggerService;
 
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,@InjectRedis() private readonly redis:Redis ) {}
 
   onModuleInit() {
     this.logger.log('AppController constructor');
@@ -24,7 +27,17 @@ export class AppController implements OnModuleInit {
   }
 
   @Get()
+  @Version('2')
   async getHello() {
-    console.log(await this.appService.getHello());
+    return await this.redis.get('token')
+  }
+
+  @Get('set')
+  @Version('2')
+  async setToken() {
+    // set('token','123456','EX', 60 * 60 * 24 * 30) 是什么意思
+    // set('token','123456','EX', 60 * 60 * 24 * 30) 的意思是设置一个token，token的值为123456，过期时间为60 * 60 * 24 * 30秒
+    // EX 是过期时间，单位是秒
+    return await this.redis.set('token','123456','EX', 60 * 60 * 24 * 30)
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from './common/config/config.module';
 import { LoggerModule } from './common/logger/logger.module';
 
@@ -11,7 +13,19 @@ const isDev = process.env.NODE_ENV === 'development';
 console.log('🚀 ~ file: app.module.ts:13 ~ isDev:', isDev);
 
 @Module({
-  imports: [ConfigModule, LoggerModule, PrismaModule],
+  imports: [ConfigModule, LoggerModule, PrismaModule, RedisModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      return {
+        type: "single", // 单例模式
+        url: `redis://${configService.get<string>('REDIS_HOST')}:${configService.get<number>('REDIS_PORT')}`,
+        options: {
+          password: configService.get<string>('REDIS_PASSWORD'),
+        }
+      };
+    },
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
