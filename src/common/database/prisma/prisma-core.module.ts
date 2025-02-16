72599cf5df5b57908a6e5f203e9245a98b8317ae
阿逼 <2553400824@qq.com>
2025-02-16 17:11:48 +0800

    feat: 实现多数据库实例识别问题

--- src/common/database/prisma/prisma-core.module.ts
+++ src/common/database/prisma/prisma-core.module.ts
@@ -17,21 +17,33 @@ export class PrismaCoreModule implements OnApplicationShutdown {
   onApplicationShutdown(signal?: string) {
     throw new Error('Method not implemented.');
   }
-  static forRoot(options: PrismaModuleOptions): DynamicModule {
+  static forRoot(_options: PrismaModuleOptions): DynamicModule {
+    const { url, options = {}, connectionName } = _options;
+    let newOptions = {
+      datasourceUrl: url,
+    };
+    // 如果options为空，则将options赋值给newOptions
+    if (!Object.keys(options).length) {
+      newOptions = {
+        ...newOptions,
+        ...options,
+      };
+    }
+    /**
+     * 这里为了实现多数据库链接，注入识别不同的实例
+     * PrismaModule.forRoot('mysql://root:123455@localhost:3306/test','mysql1')
+     * PrismaModule.forRoot('postgresql://root:123455@localhost:5432/test','postgresql1')
+     */
+    const providerName = connectionName || PRISMACLIENT;
     // 实现client连接
     const prismaClientProviders: Provider = {
-      provide: PRISMACLIENT,
+      provide: providerName,
       useFactory() {
-        const url = options.url;
         const dbType = getDBTYpe(url);
-        const _options = {
-          datasourceUrl: options.url,
-          ...options.options,
-        };
         if (dbType === 'mysql') {
-          return new PrismaMysqlClient(_options);
+          return new PrismaMysqlClient(newOptions);
         } else if (dbType === 'postgresql') {
-          return new PrismaPostgresqlCLient(_options);
+          return new PrismaPostgresqlCLient(newOptions);
         } else {
           throw new Error(`不支持的链接数据库类型 ${dbType}`);
         }
--- src/common/database/prisma/prisma-options.interface.ts
+++ src/common/database/prisma/prisma-options.interface.ts
@@ -3,4 +3,5 @@ import { Prisma } from '@prisma/client';
 export interface PrismaModuleOptions {
   url?: string;
   options?: Prisma.PrismaClientOptions;
+  connectionName?: string;
 }
